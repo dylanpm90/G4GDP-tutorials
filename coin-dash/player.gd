b2f505ed0cca869b6fa1c8c4c76c681ca51bc6f0
dylanpm90 <dylan.p.mackay@gmail.com>
2026-05-01 13:15:11 -0700

    player update
    added collision, movement and animations for player
    
    wrote a function for starting position and animation
    
    wrote a function for player death

--- coin-dash/player.gd
+++ coin-dash/player.gd
@@ -4,6 +4,15 @@ extends Area2D
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
 
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "idle"
+
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,4 +20,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# input and movement
+	velocity = Input.get_vector(
+		"ui_left", 
+		"ui_right", 
+		"ui_up", 
+		"ui_down"
+		)
+	position += velocity * speed * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	# animations
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
